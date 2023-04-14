12baccba09ae24cf89bfd1163c0c52ebe16a8d76
Paul Foster <restofpaul@gmail.com>
2023-04-14 23:39:00 +0000

    answer

--- src/App.java
+++ src/App.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        System.out.print("What's 8 X 3? ");
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
 
-        
+        int answer = scanner.nextInt();
+
+        System.out.println("83" + answer);
     }
 }
