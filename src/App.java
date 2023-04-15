36a2a0f74351b1483b53e7aceaebd9c198554722
Paul Foster <restofpaul@gmail.com>
2023-04-15 00:17:29 +0000

    answers

--- src/App.java
+++ src/App.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
 
-        System.out.println("Hello, World!");
+        System.out.print("Enter a 4 digit number: ");
 
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String number = scanner.nextLine();
+
+        String digit1, digit2, digits34;
+
+        digit1 = number.substring(0, 1);
+        digit2 = number.substring(1,2);
+        digits34 = number.substring(2, 4);
+
+        int first, second, third;
+
+        first = Integer.parseInt(digit1);
+        second = Integer.parseInt(digit2);
+        third = Integer.parseInt(digits34);
+
+        if(first * second == third) {
+            System.out.println("SUCCESS! " + first + " X " + second + " == "  + third);
+        }
+        else{
+            System.out.println("FAIL - " + first + " X " + second + " != " + third);
+        }
     }
 }
